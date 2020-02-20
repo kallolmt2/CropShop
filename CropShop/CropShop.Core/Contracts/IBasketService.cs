eb5d24644f6641af83f7b37818da0418a993ac9c
kallolmt2 <30737874+kallolmt2@users.noreply.github.com>
2020-02-20 11:18:30 +0530

    Finished Basket Service

---
+++ CropShop/CropShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,19 @@
+﻿using CropShop.Core.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CropShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+
+    }
+}
