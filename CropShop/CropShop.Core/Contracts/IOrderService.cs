7a78a044b0cd09c695e580a016fd26119f2c7975
kallolmt2 <30737874+kallolmt2@users.noreply.github.com>
2020-02-25 20:17:03 +0530

    order manager created

--- CropShop/CropShop.Core/Contracts/IOrderService.cs
+++ CropShop/CropShop.Core/Contracts/IOrderService.cs
@@ -11,5 +11,8 @@ namespace CropShop.Core.Contracts
     public interface IOrderService
     {
         void CreateOrder(Order baseOrder, List<BasketItemViewModel> basektItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
     }
 }
